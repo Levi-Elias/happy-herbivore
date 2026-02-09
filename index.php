eea15532c149d5ccdd89242ea975bcc6541909f0
basspies <haroldspies@hotmail.com>
2026-02-09 14:30:25 +0100

    logo

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 include 'assets/includes/conn.php';
+include 'assets/includes/header.php';
 ?>
 
+<div class="index-container">
+    <div class="logo">
+        <img src="assets/img/logo.png" alt="Happy Herbivore" class="logo">
+    </div>
+</div>
+
+
+
+
+
+
+
+<?php
+include 'assets/includes/footer.php';
+?>
