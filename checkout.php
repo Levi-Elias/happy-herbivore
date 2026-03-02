488e83b1486216d5609fbac22a2f4144dee664e8
Levi-Elias <le.sastro@outlook.com>
2026-03-02 14:44:14 +0100

    lol

--- checkout.php
+++ checkout.php
@@ -37,9 +37,11 @@ include 'assets/includes/header.php';
             </div>
 
             <div class="summary-actions">
-                <button class="btn-gray" onclick="window.location.href='menu.php'" data-t="add_more">Add More
+                <button class="btn-more" onclick="window.location.href='menu.php'" data-t="add_more">Add More
                     Items</button>
-                <button class="btn-orange" id="btn-checkout" onclick="initiatePayment()" data-t="place_order">Place
+                <button class="btn-cancelo" onclick="cancelOrder()" data-t="cancel_order">Cancel 
+                    Order</button>
+                <button class="btn-place" id="btn-checkout" onclick="initiatePayment()" data-t="place_order">Place
                     Order</button>
             </div>
         </div>
