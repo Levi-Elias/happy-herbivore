6268c8d358303642fedaecac53acc16ffbb61a53
Levi-Elias <le.sastro@outlook.com>
2026-03-09 13:16:04 +0100

    bon fail

--- assets/includes/conn.php
+++ assets/includes/conn.php
@@ -1,21 +1,22 @@
 <?php
-// $servername = "localhost";
-// $dbname = "kiosk";
-// $username = "root";
-// $password = "";
-
 $servername = "localhost";
-$dbname = "u240669_kiosk";
-$username = "u240669_kiosk";
-$password = "DeHQpF5p9KQgrPph7zgj";
+$dbname = "kiosk";
+$username = "root";
+$password = "";
+
+// $servername = "localhost";
+// $dbname = "u240669_kiosk";
+// $username = "u240669_kiosk";
+// $password = "DeHQpF5p9KQgrPph7zgj";
 
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // echo "Connected successfully";
-} catch (PDOException $e) {
+// echo "Connected successfully";
+}
+catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
 ?>
